4101080e9dea3906a018b034d774a48c2b15f1da
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-19 22:28:52 +0700

    feat: multiple contacts and console log

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const contact = {
-  id: 1,
-  name: "M Haidar Hanif",
-  age: 30,
-  email: "me@mhaidarhanif.com",
-  phone: "+62-812-345-678",
-  address: {
-    street: "Jl. Mana",
-    city: "Bandung",
-    country: "Indonesia",
+const contacts = [
+  {
+    id: 1,
+    name: "M Haidar Hanif",
+    age: 30,
+    email: "me@mhaidarhanif.com",
+    phone: "+62-812-345-678",
+    website: "https://mhaidarhanif.com",
+    labels: ["friend", "work"],
+    address: {
+      street: "Jl. Mana",
+      city: "Bandung",
+      state: "West Java",
+      country: "Indonesia",
+    },
+    isFavorited: true,
   },
-};
+  {
+    id: 2,
+    name: "Elon Musk",
+    age: 50,
+    email: "elon@tesla.com",
+    phone: "+1-234-567-890",
+    website: "https://tesla.com",
+    labels: ["work"],
+    address: {
+      street: "",
+      city: "Austin",
+      state: "Texas",
+      country: "United States",
+    },
+    isFavorited: false,
+  },
+];
+
+console.log(contacts);
