37eff7b7eda6a6fd2a42f334b39fab6d098cf0d8
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-19 22:20:13 +0700

    feat: contact data

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-console.log("Hello World!");
-
-const fullName = "Haidar";
-const age = 30;
-const homeAddress = "Bandung";
-const isMentor = true;
+const contact = {
+  id: 1,
+  name: "M Haidar Hanif",
+  age: 30,
+  email: "me@mhaidarhanif.com",
+  phone: "+62-812-345-678",
+  address: {
+    street: "Jl. Mana",
+    city: "Bandung",
+    country: "Indonesia",
+  },
+};
